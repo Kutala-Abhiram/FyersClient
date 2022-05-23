a88ca8333a5e3abb8458a893197914afcb9f2cde
Kutala-Abhiram <kabhiram1998@gmail.com>
2022-05-23 22:04:26 +0530

    Handle exception

--- src/FyersConnect.js
+++ src/FyersConnect.js
@@ -10,24 +10,33 @@ module.exports = class FyersConnect {
     this.token = token;
   }
 
-  subscribeSymbol(symbol, callback) {
+  subscribeSymbol(symbol, callback, retry = 0) {
     let reqBody = { token: this.token, dataType: updateSymbol, symbol: [symbol] };
       
-    fyers.fyers_connect(reqBody, (websocketData) => {
-      let parsedData = JSON.parse(websocketData);
-      if(parsedData && parsedData["s"] === "ok" && parsedData["d"] !== null && parsedData["d"]["7208"] !== null) {
-        let firstMarketData = parsedData["d"]["7208"][0];
+    try {
+      fyers.fyers_connect(reqBody, (websocketData) => {
+        let parsedData = JSON.parse(websocketData);
+        if(parsedData && parsedData["s"] === "ok" && parsedData["d"] !== null && parsedData["d"]["7208"] !== null) {
+          let firstMarketData = parsedData["d"]["7208"][0];
 
-        if(firstMarketData) {
-          if (firstMarketData["s"] == "ok" && firstMarketData["v"] && firstMarketData["v"]["lp"]) {
-            callback(firstMarketData["v"]);
-          } else {
-            console.log("wrong data from fyers");
+          if(firstMarketData) {
+            if (firstMarketData["s"] == "ok" && firstMarketData["v"] && firstMarketData["v"]["lp"]) {
+              callback(firstMarketData["v"]);
+            } else {
+              console.log("wrong data from fyers");
+            }
           }
         }
+      }); 
+    } catch(e) {
+      console.log(e);
+      if(retry < 3) {
+        setTimeout(() => {
+          this.subscribeSymbol(symbol, callback, retry + 1); 
+        }, 5000);
       }
-    });
-  }
+    }
+ }
 
   unsubscribeSymbol(symbol) {
     let reqBody = { token: this.token, dataType: updateSymbol, symbol: [symbol] };
